1389381c7abd271a871cf654e603486727f208d2
Prasan Nimantha <nimoprasan@gmail.com>
2021-04-01 19:57:18 +0530

    started building the geocoder client

---
+++ geocoder-client/src/main/java/com/springtraining/geocoderclient/GeocoderClientApplication.java
@@ -0,0 +1,13 @@
+package com.springtraining.geocoderclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocoderClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeocoderClientApplication.class, args);
+	}
+
+}
---
+++ geocoder-client/src/test/java/com/springtraining/geocoderclient/GeocoderClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtraining.geocoderclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocoderClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
