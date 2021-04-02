224cc385ff9faf0a8512133faf35babda50547fc
Prasan Nimantha <nimoprasan@gmail.com>
2021-04-02 11:52:52 +0530

    started building the geocoder client

---
+++ geocoder-client/src/main/java/com/springtraining/geocoderclient/json/Response.java
@@ -0,0 +1,31 @@
+package com.springtraining.geocoderclient.json;
+import java.util.List;
+
+public class Response {
+    private List<Result> results;
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<Result> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+    public Location getLocation() {
+        return results.get(0).getGeometry().getLocation();
+    }
+
+    public String getFormattedAddress() {
+        return results.get(0).getFormattedAddress();
+    }
+}
